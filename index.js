46d43f435578acafd74af607f0da593f0383731d
iQiaFUN <101959120+iQiaFUN@users.noreply.github.com>
2022-05-02 21:50:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const request = require("sync-request");
+const motd_api = 'https://api.iqia.fun/motd/?';
+const cmd = '/motdpe';
+
+function getText(e) {
+    var rt = '';
+    for (i in e.message) {
+        switch (e.message[i].type) {
+            case "text":
+                rt += e.message[i].text;
+                break;
+        }
+    }
+    return rt;
+}
+
+function motd(u){
+	let obj = request('GET',u);
+        if(obj.statusCode == 200){
+            let data = JSON.parse(obj.getBody('utf8'));
+            if(data.status == 'online'){
+                return [
+                    `[MCBE服务器信息]\n\n`,
+                    `协议版本：${data.agreement}\n`,
+                    `游戏版本：${data.version}\n`,
+					`游戏模式：${data.gamemode}\n`,
+                    `描述文本：${data.motd}\n`,
+                    `在线人数：${data.online}/${data.max}\n`,
+                    `网络延迟：${data.delay}ms`,
+					`API 由 iQiaFUN 提供`
+                ]
+            }else{
+                return '服务器离线';
+            }
+        }else{
+            return 'api连接失败，经检查api是否可以访问';
+        }
+}
+
+function onStart(api){
+    api.listen('onMainMessageReceived',(e)=>{
+		let text = getText(e);
+		let pt = text.split(' ');
+		if(pt[0]==cmd){
+			switch(pt.length){
+				case 2:
+					var u_api = `${motd_api}ip=${pt[1]}&port=19132`;
+					break;
+				case 3:
+					var u_api = `${motd_api}ip=${pt[1]}&port=${pt[2]}`;
+					break;
+			}
+			let str = motd(u_api);
+			e.reply(str);
+		}
+	});
+}
+
+
+function onStop(){
+}
+
+module.exports = {
+    onStart,
+    onStop
+};
